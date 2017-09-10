a58c5955b6d14e5efe70309b78dad9fbc7c29a46
mateuszarchicinski <mateusz.archicinski@gmail.com>
2017-09-10 21:37:43 +0200

    Refactoring of current components Sass&Pug

--- src/js/main.js
+++ src/js/main.js
@@ -3,5 +3,21 @@
 
 console.info('JavaScript running...');
 
+var mainHeader = document.querySelector('.main-header');
+
+window.addEventListener('click', function () {
+    if (mainHeader.classList.contains('hide')) {
+        mainHeader.classList.remove('hide');
+        mainHeader.classList.add('show');
+        status = true;
+
+        return;
+    }
+
+    mainHeader.classList.remove('show');
+    mainHeader.classList.add('hide');
+    status = false;
+});
+
 }());
 //# sourceMappingURL=main.js.map
--- src/js/main.rollup.js
+++ src/js/main.rollup.js
@@ -1,1 +1,19 @@
 console.info('JavaScript running...');
+
+
+const mainHeader = document.querySelector('.main-header');
+
+
+window.addEventListener('click', () => {
+    if (mainHeader.classList.contains('hide')) {
+        mainHeader.classList.remove('hide');
+        mainHeader.classList.add('show');
+        status = true;
+
+        return;
+    }
+
+    mainHeader.classList.remove('show');
+    mainHeader.classList.add('hide');
+    status = false;
+});
